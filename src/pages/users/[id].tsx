10740d142af0c8d403c079ea1dea4e7b9ad55026
Bruno <bruno0907@gmail.com>
2021-08-13 12:08:27 -0300

    feat: added User page component first layout

--- src/pages/users/[id].tsx
+++ src/pages/users/[id].tsx
@@ -1,5 +1,21 @@
+import { Header } from '../../components/Header'
+import { SideMenu } from '../../components/SideMenu'
+
+import { 
+  Flex,
+  Heading
+} from '@chakra-ui/react'
+
 export default function User() {
   return (
-    <h1>Cliente</h1>
+    <Flex p="8">
+      <Header title="Marka | Cliente"/>
+      <Flex flexDir="row">
+        <SideMenu />
+        <Flex p="8">
+          <Heading>Tela dos dados do Cliente</Heading>
+        </Flex>
+      </Flex>
+    </Flex>
   )
 }
