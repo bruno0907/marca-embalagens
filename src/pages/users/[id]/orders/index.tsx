386a223263c3468a99cfc229f88862ce59e5f519
Bruno <bruno0907@gmail.com>
2021-11-29 09:11:33 -0300

    chore: Componentized user orders list

--- src/pages/users/[id]/orders/index.tsx
+++ src/pages/users/[id]/orders/index.tsx
@@ -1,31 +1,26 @@
 import { useRef } from 'react'
+
 import Head from 'next/head'
 import { useRouter } from "next/router"
 
-import { useUserOrdersQuery } from "../../../../hooks/useUserOrdersQuery"
-import { useUserQuery } from '../../../../hooks/useUserQuery'
+import { useReactToPrint } from 'react-to-print'
 
 import { Authenticated } from '../../../../components/Layout/Authenticated'
 import { Header } from '../../../../components/Header'
 import { Content } from '../../../../components/Layout/Content'
-import { handleFormatDate } from '../../../../utils/handleFormatDate'
-import { handleFormatPrice } from '../../../../utils/handleFormatPrice'
-import { useReactToPrint } from 'react-to-print'
 import { Divider } from '../../../../components/Layout/Divider'
+import { OrderHeader } from './components/OrderHeader'
+import { OrderItemsList } from './components/OrderItemsList'
+
+import { useUserOrdersQuery } from "../../../../hooks/useUserOrdersQuery"
+import { useUserQuery } from '../../../../hooks/useUserQuery'
 
 import { 
   Text,
-  Button,
-  Table,
-  Box,
-  Thead,
-  Th,
-  Tbody,
-  Tr,
-  Td,
+  Button,  
+  Box,  
   Heading,
   Stack,
-  HStack,
   Center,
   Spinner,
 } from "@chakra-ui/react"
@@ -35,13 +30,13 @@ export default function UserOrders() {
   const router = useRouter()
   const id = router.query.id  
 
-  const ref = useRef<HTMLDivElement>(null)
+  const OrderstoPrintRef = useRef<HTMLDivElement>(null)
 
   const user = useUserQuery(String(id))
   const orders = useUserOrdersQuery(String(id))  
 
   const handlePrintOrders = useReactToPrint({
-    content: () => ref.current,
+    content: () => OrderstoPrintRef.current,
   })  
 
   return (
@@ -51,7 +46,9 @@ export default function UserOrders() {
       </Head>
 
       <Authenticated>
-        <Header withGoBack title={`Pedidos de ${user.data?.nome}`}>
+        <Header 
+          withGoBack 
+          title={user.data?.nome && `Pedidos de ${user.data.nome}`}>
           <Button leftIcon={<FiPrinter />} colorScheme="blue"  onClick={handlePrintOrders}>
             Imprimir
           </Button>
@@ -60,13 +57,13 @@ export default function UserOrders() {
         <Divider />
 
         <Content p="0">
-          <Box ref={ref} m="8">
+          <Box ref={OrderstoPrintRef} m="8">
             <Heading 
               fontSize="2xl" 
               mb="8" 
               display="none"
               sx={{ "@media print": { display: 'block' } }}
-            > Todos os pedidos de {user.data?.nome}            
+            > Todos os pedidos de {user.data?.nome || ''}            
             </Heading>
             
             { orders.isLoading || user.isLoading ? (
@@ -81,42 +78,15 @@ export default function UserOrders() {
                   return (                
                     <Box key={order.id}>
                       <Stack borderWidth="thin" p="4" borderRadius="md" spacing={6}>
-                        <HStack spacing={6} w="100%" justify="space-between">
-                          <Box>
-                            <Text>Pedido: <strong>{order.numero_pedido}</strong></Text>
-                          </Box>
-                          <Box>
-                            <Text>Data de emissão: <strong>{handleFormatDate(order.created_at)}</strong></Text>
-                          </Box>
-                          <Box>
-                            <Text>Data de entrega: <strong>{handleFormatDate(order.data_entrega)}</strong></Text>
-                          </Box>
-                        </HStack>
+                        <OrderHeader
+                          orderNumber={order.numero_pedido}
+                          createdAt={order.created_at}
+                          deliveryDate={order.data_entrega}
+                        />
                         
                         <Heading fontSize="large" textAlign="center">Descrição do pedido:</Heading>
                         
-                        <Table variant="striped" size="sm">
-                          <Thead>
-                            <Tr>
-                              <Th w="20" textAlign="center">Qtd</Th>
-                              <Th>Produtos</Th>
-                              <Th w="40" textAlign="end">Valor unitário</Th>
-                              <Th w="40" textAlign="end">Valor total</Th>
-                            </Tr>
-                          </Thead>
-                          <Tbody>                       
-                            { order.pedido?.map(item => {
-                              return (
-                                <Tr key={item.produto}>
-                                  <Td textAlign="center">{item.quantidade}</Td>
-                                  <Td>{item.produto}</Td>
-                                  <Td textAlign="end">{handleFormatPrice(item.valor_unitario)}</Td>
-                                  <Td textAlign="end">{handleFormatPrice(item.valor_total)}</Td>
-                                </Tr>
-                              )})
-                            }
-                          </Tbody>
-                        </Table>
+                        <OrderItemsList orderItems={order?.pedido}/>
                       </Stack>
                     </Box>    
                   )
