23088dd0a721799af021cdd94994c41729d52701
Bruno <bruno0907@gmail.com>
2021-11-29 09:41:54 -0300

    chore: Moved Supplier Information and Addresses to components from supplier-to-print page

--- src/pages/suppliers/[id]/supplier-to-print/index.tsx
+++ src/pages/suppliers/[id]/supplier-to-print/index.tsx
@@ -9,13 +9,14 @@ import { Divider } from "../../../../components/Layout/Divider"
 import { Content } from "../../../../components/Layout/Content"
 import { Header } from "../../../../components/Header"
 
+import { Information } from "./components/Information"
+import { Addresses } from "./components/Addresses"
+
 import { useSupplierQuery } from "../../../../hooks/useSupplierQuery"
 import { useAddressesQuery } from "../../../../hooks/useAddressesQuery"
 
 import { 
-  Box,  
-  VStack,
-  HStack,
+  Box,
   Heading,
   Text,
   Button,  
@@ -27,13 +28,12 @@ import { FiPrinter } from "react-icons/fi"
 
 export default function SupplierToPrint() {
   const router = useRouter()
-  const userId = router.query.id
+  const supplierId = router.query.id
 
   const ref = useRef<HTMLDivElement>(null)
 
-  const supplier = useSupplierQuery(String(userId))
-  const addresses = useAddressesQuery(String(userId))
-  
+  const supplier = useSupplierQuery(String(supplierId))
+  const addresses = useAddressesQuery(String(supplierId))  
 
   const handlePrint = useReactToPrint({
     content: () => ref.current,
@@ -59,136 +59,26 @@ export default function SupplierToPrint() {
           <Box ref={ref} p="8">
             <Heading fontSize="lg" mb="4">Dados Cadastrais</Heading>
 
-            { !supplier.data ? null 
-              : supplier.isLoading || supplier.isFetching ? (
-                  <Center>
-                    <Spinner color="blue.500"/>
-                  </Center>
-                )
-              : supplier.isError ? (
-                  <Text>Erro ao carregar os dados do cliente...</Text>
-                )
-              : (
-                <VStack spacing={3} align="start" borderWidth="1px" borderRadius="md" p="4">
-                  <HStack w="100%" spacing={3} align="flex-start" justify="space-between">
-                    <Box>
-                      <Text fontSize="x-small" fontWeight="bold">Nome:</Text>
-                      <Text fontSize="sm">{supplier.data?.nome}</Text>            
-                    </Box>
-                    { supplier.data?.razao_social && (
-                      <Box>
-                        <Text fontSize="x-small" fontWeight="semibold">Razão social:</Text>
-                        <Text fontSize="sm">{supplier.data?.razao_social}</Text>
-                      </Box>
-                    )}
-                    { supplier.data?.cpf_cnpj && (
-                      <Box>
-                        <Text fontSize="x-small" fontWeight="semibold">
-                          {supplier.data?.razao_social ? 'CNPJ' : 'CPF'}
-                        </Text>
-                        <Text fontSize="sm">
-                          {supplier.data?.cpf_cnpj}
-                        </Text>
-                      </Box>
-                    )}
-                  </HStack>
-                  <Box>
-                    <Text fontSize="x-small" fontWeight="bold">Produto/Serviço:</Text>
-                    <Text fontSize="sm">{supplier.data?.produto}</Text>            
-                  </Box>
-                  <HStack w="100%" spacing={3} align="flex-start" justify="space-between">
-                    <Box>
-                      <Text fontSize="x-small" fontWeight="bold">Telefone:</Text>
-                      <Text fontSize="sm">
-                        {supplier.data?.telefone}
-                      </Text>            
-                    </Box>
-                    { supplier.data?.celular && (
-                        <Box>
-                          <Text fontSize="x-small" fontWeight="bold">Celular:</Text>
-                          <Text fontSize="sm">
-                            {supplier.data?.celular}
-                          </Text>            
-                        </Box>
-                    )}
-                    <Box>
-                      <Text fontSize="x-small" fontWeight="bold">E-mail:</Text>
-                      <Text fontSize="sm">
-                        {supplier.data?.email}
-                      </Text>            
-                    </Box>
-                    { supplier.data?.contato && (
-                        <Box>
-                          <Text fontSize="x-small" fontWeight="bold">Contato:</Text>
-                          <Text fontSize="sm">
-                            {supplier.data?.contato}
-                          </Text>            
-                        </Box>
-                    )}
-                  </HStack>
-              </VStack>
-            )}
+            { supplier.isLoading || supplier.isFetching 
+              ? <Center>
+                  <Spinner color="blue.500"/>
+                </Center>
+              : supplier.isError 
+              ? <Text>Erro ao carregar os dados do cliente...</Text>                
+              : <Information supplier={supplier.data}/>              
+            }
 
             <Divider/>
 
             <Heading fontSize="lg" mb="4">Endereços</Heading>
 
-            { !addresses.data ? null 
-              : addresses.isLoading || addresses.isFetching ? (
-                  <Center>
+            { addresses.isLoading || addresses.isFetching 
+              ? <Center>
                     <Spinner color="blue.500"/>
                   </Center>
-                )
-              : addresses.isError ? (
-                  <Text>Erro ao carregar os endereços do cliente;;;</Text>
-                )
-              : (
-                <VStack spacing={3} align="start">
-                  { addresses.data?.map(address => {
-                    return (  
-                      <Box 
-                        key={address.id} 
-                        w="100%"
-                        borderWidth="1px" 
-                        borderRadius="md" 
-                        p="4"
-                      >
-                        <HStack 
-                          w="100%" 
-                          spacing={3}
-                          align="flex-start" 
-                          justify="space-between" 
-                        >
-                          <Box>
-                            <Text fontSize="x-small" fontWeight="bold">Endereço:</Text>
-                            <Text fontSize="sm">
-                              {address.endereco}
-                            </Text>
-                          </Box>
-                          <Box>
-                            <Text fontSize="x-small" fontWeight="bold">Bairro:</Text>
-                            <Text fontSize="sm">
-                              {address.bairro}
-                            </Text>
-                          </Box>
-                          <Box>
-                            <Text fontSize="x-small" fontWeight="bold">CEP:</Text>
-                            <Text fontSize="sm">
-                              {address.cep}
-                            </Text>
-                          </Box>
-                          <Box>
-                            <Text fontSize="x-small" fontWeight="bold">Cidade/UF:</Text>
-                            <Text fontSize="sm">
-                              {address.cidade}/{address.estado}
-                            </Text>
-                          </Box>
-                        </HStack>
-                      </Box>
-                    )
-                  })}
-                </VStack>
-              )
+              : addresses.isError 
+              ? <Text>Erro ao carregar os endereços do cliente...</Text>
+              : <Addresses addresses={addresses.data}/>              
             }
           </Box>
         </Content>
