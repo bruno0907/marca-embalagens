020530589d05f2d8deaade57183000df699400f7
Bruno <bruno0907@gmail.com>
2021-12-20 11:04:07 -0300

    chore: added test config file. added new options to jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   testIgnorePatterns: ["/node_modules/", "/.next/"],
+  setupFilesAfterEnv: [
+    "<rootDir>/src/__tests__/setupTests.ts"
+  ],
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
-  }
+  },
+  testEnvironment: 'jsdom',
 }
