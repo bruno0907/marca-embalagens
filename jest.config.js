7ead981fd78ef468b17c4dc46ad7c908181cbf35
Bruno <bruno0907@gmail.com>
2021-12-20 11:18:24 -0300

    fix: jest testIgnorePatterns fix. Added babel config file

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  testIgnorePatterns: ["/node_modules/", "/.next/"],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   setupFilesAfterEnv: [
-    "<rootDir>/src/__tests__/setupTests.ts"
+    '<rootDir>/src/__tests__/setupTests.ts'
   ],
   transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
   },
   testEnvironment: 'jsdom',
 }
---
+++ src/components/__tests__/Header.spec.tsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react'
+import { Header } from '../Header'
+
+test('It should render correctly', () => {
+  render(
+    <Header />
+  )
+}) 
