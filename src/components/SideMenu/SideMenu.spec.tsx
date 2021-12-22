284884a561302bbb4d84790e140fc3cd2c09e5f3
Bruno <bruno0907@gmail.com>
2021-12-21 22:00:35 -0300

    test: should signOut if Sair button is pressed

--- src/components/SideMenu/SideMenu.spec.tsx
+++ src/components/SideMenu/SideMenu.spec.tsx
@@ -21,11 +21,28 @@ jest.mock('../../hooks/useSignOutMutation', () => {
   }
 })
 
+const useSignOutMutation = jest.spyOn(require('../../hooks/useSignOutMutation'), 'useSignOutMutation')
+
 describe('SideMenu', () => {
   it('should render correcly', () => {
     const { container } = render(<SideMenu />)
 
     expect(container).toBeInTheDocument()
   })
-  
+
+  it('should signOut if Sair button is pressed', () => {
+    render(<SideMenu/>)
+
+    const signOutButton = screen.getByRole('button', { name: 'Sair' })
+
+    const handleSignOutMock = useSignOutMutation.mockImplementationOnce(() => {
+      return {
+        mutate: jest.fn()
+      }
+    })
+
+    fireEvent.click(signOutButton)
+
+    expect(handleSignOutMock).toHaveBeenCalled()
+  })
 })
