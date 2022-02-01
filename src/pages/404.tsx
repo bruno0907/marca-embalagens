2a6f1ba49b007c96e182b0d01acb537add696cc9
Bruno <bruno0907@gmail.com>
2022-02-01 15:29:52 -0300

    chore: 404 styling

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,24 @@
 import Link from 'next/link'
 
-import { Center, Heading, Link as ChakraLink } from "@chakra-ui/layout";
+import { Center, Heading, Link as ChakraLink, Stack, Text } from "@chakra-ui/layout";
+import { Button } from '@chakra-ui/react';
 
 export default function NotFound() {
   return(
-    <Center h="100vh" flexDir="column">
-      <Heading mb="8">Página não encontrada</Heading>
-      <Link href="/sign-in" passHref>
-        <ChakraLink>Voltar</ChakraLink>
-      </Link>
+    <Center h="100vh" flexDir="column">     
+      <Stack spacing={6} align="center" justify="center">
+        <Stack align="center">
+          <Heading>Oops! 404</Heading>
+          <Text fontWeight="medium">A página procurada não pôde ser encontrada</Text>
+        </Stack>
+        <Link href="/sign-in" passHref>
+          <Button as={ChakraLink} colorScheme="blue" _hover={{ 
+            textDecor: 'none',
+            bgColor: 'blue.600'
+          } }>Voltar</Button>
+        </Link>
+
+      </Stack>
     </Center>
   )
 }
